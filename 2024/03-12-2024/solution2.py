4cc52c05ff82017013695fad039f8aa255f35172
Nisar Khan <nihakh987@gmail.com>
2024-12-03 06:53:20 +0000

    day 3

---
+++ 2024/03-12-2024/solution2.py
@@ -0,0 +1,65 @@
+
+
+example = "example.txt"
+inputFile = "input.txt"
+example2 = "example2.txt"
+f = open(inputFile,'r')
+total = 0
+text = ""
+for line in f:
+    text += line
+line = text
+enabled = True
+for i in range(0,len(line) - 7,1):
+    if line[i:i + 4] == "do()":
+        enabled = True
+    if line[i:i+7] == "don't()":
+        enabled = False
+    if line[i:i + 4] == "mul(" and enabled:
+        commaFound = False
+        bracketFound = False
+        continueFlag = False
+        j = i + 4
+        first = ""
+        for k in range(j,len(line) - 3,1):
+            if k >= j + 4 and not commaFound:
+                continueFlag = True
+                break
+            if not line[k].isdigit() and line[k] != ',':
+                continueFlag = True
+                break
+            if line[k] == ',':
+                commaFound = True
+                if k == j:
+                    continueFlag = True
+                    break
+                first = line[j:k]
+                j = k + 1
+                break
+        if continueFlag:
+            continue
+        second = ""
+        if j >= len(line) - 1:
+            continue
+        for k in range(j,len(line) - 1,1):
+            if k >= j + 4 and not bracketFound:
+                continueFlag = True
+                break
+            if not line[k].isdigit() and line[k] != ')':
+                continueFlag = True
+                break
+            if line[k] == ')':
+                bracketFound = True
+                if k == j:
+                    continueFlag = True
+                    break
+                second = line[j:k]
+                j = k + 1
+                break
+        if continueFlag:
+            continue
+        first = int(first)
+        second = int(second)
+        total += first * second
+
+print(total)
