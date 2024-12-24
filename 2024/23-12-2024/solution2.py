27eee0e1c3392731896e2ca746f4c4961b6a095c
Nisar Khan <nihakh987@gmail.com>
2024-12-24 14:19:03 +0000

    day 23

--- 2024/23-12-2024/solution2.py
+++ 2024/23-12-2024/solution2.py
@@ -8,11 +8,14 @@ example = "example.txt"
 inputFile = "input.txt"
 connections = []
 triples = []
+computers = set()
 f = open(inputFile,'r')
 for line in f:
     line = line.strip()
     first = line.split('-')[0]
     second = line.split('-')[1]
+    computers.add(first)
+    computers.add(second)
     firstConnections = set()
     secondConnections = set()
     for connectSet in connections:
@@ -38,11 +41,31 @@ for line in f:
     connection.add(first)
     connection.add(second)
     connections.append(connection)
+bigConnectionSets = []
+for triple in triples:
+    for c in computers:
+        if c in triple:
+            continue
+        matches = 0
+        for conn in connections:
+            if c in conn and getOtherConnection(c,conn) in triple:
+                matches += 1
+        if matches == len(triple):
+            triple.add(c)
 
-count = 0
-for tripleSet in triples:
-    for c in tripleSet:
-        if c[0] == 't':
-            count += 1
-            break
-print(count)
+maxSize = 0
+for triple in triples:
+    maxSize = max(maxSize,len(triple))
+chosen = []
+for triple in triples:
+    if len(triple) == maxSize:
+        for c in triple:
+            chosen.append(c)
+        break
+chosen.sort()
+s = ""
+for c in chosen:
+    s += c
+    s += ","
+s = s[:len(s) - 1]
+print(s)
